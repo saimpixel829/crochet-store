51933a9ffb9abb0c57be25607ef8f603df802738
saimpixel829 <saimpixel829@gmail.com>
2026-09-08 19:03:55 +0500

    Create Home component for Crochet Store page

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: '2rem', fontFamily: 'sans-serif', textAlign: 'center' }}>
+      <h1>🧶 Welcome to Crochet Store</h1>
+      <p>Your custom handmade crochet boutique is live!</p>
+    </main>
+  )
+}
